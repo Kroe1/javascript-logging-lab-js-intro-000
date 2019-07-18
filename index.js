c697533afa3a5b2c7f909f7f7c8c479acb89f665
Kelson Roe <kelsonmroe@gmail.com>
2019-07-18 21:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.error("HALP!")
 console.log("I would be a logger!")
 console.warn("This is a warning!")
+var word
+word='bird'
+console.log(word)
+word='dog'
+console.log(word)
+var word='bird'
+window.word
+
